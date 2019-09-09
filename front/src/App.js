b7d57b7b507358482a4c78865658f4bd865e9a71
takuyanin <ftakuyamax0701@gmail.com>
2019-09-09 15:39:44 +0900

    Change how to display background image

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MainContainer from './Components/MainContainer'
-import bgImage from '../public/image.jpg'
 
 class App extends React.Component {
   render () {
-    const style = {backgroundImage:`url(${bgImage})`}
     return (
-      <div className='App' style={style}>
+      <div className='App'>
         <MainContainer />
       </div>
     )
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-                url: false,
+                url: true,
                 importLoaders: 2
             }
           },
